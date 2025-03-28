fd2d90c3c0f66dcf486263364e724bc115996723
Ysamwel <yvonneatieno2000@gmail.com>
2025-03-28 08:12:45 +0300

    passenger

---
+++ passenger.js
@@ -0,0 +1,37 @@
+//document.getElementById("count-display").innerText = 9
+
+//find way to store count with a variable(count)
+//let firstBatch = 6
+//let secondBatch = 10
+//let count = firstBatch + secondBatch
+
+//let count = 10 / 5
+
+//console.log(count)
+
+//create 2 variabls myAge and humanDogRatio
+
+//let myAge = 6
+//let humanDogRatio = 5
+//multiply the two together and store the results in myDogAge
+//let myDogAge = myAge * humanDogRatio
+// log myDogAge to the console
+//console.log(myDogAge)
+
+//let count = 3
+//count = count + 1
+//console.log(count)
+
+//1. Create a variable bonusPoints. Initialize it as 50. Increase to 100
+
+//let bonusPoints = 50
+//console.log(bonusPoints)
+//bonusPoints = bonusPoints + 50
+//console.log(bonusPoints)
+// Decrease it down to 25 and increase it to 70
+//bonusPoints = bonusPoints - 75
+//console.log(bonusPoints)
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints)
+
+
